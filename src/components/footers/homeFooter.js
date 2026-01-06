300a2ea70e5d6f524d48277c76ca2ee507344008
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-01-06 13:34:13 -0500

    Add homeFooter component with copyright information

---
+++ src/components/footers/homeFooter.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+    <footer className="footer">
+        <h1 className="footer-title">Direct Connection</h1>
+        <div className="footer-copyright">
+      <p className="footer-copy-p">&copy; 2026 Direct Connection - 
+         Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
+    </div>
+        </footer>
+        </div>
+  );
+};
+export default Footer;

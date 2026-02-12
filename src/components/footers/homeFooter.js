9bf25e04def69c2e2367e19af41161b18b82b218
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-02-12 15:04:57 -0500

    Update homeFooter.js

--- src/components/footers/homeFooter.js
+++ src/components/footers/homeFooter.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <h1 className="footer-title">Direct Connection</h1>
         <div className="footer-copyright">
       <p className="footer-copy-p">&copy; 2026 Direct Connection - 
-         Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
+         Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.trafficbarriersolutions.com" target="_blank" rel="noopener noreferrer">Traffic & Barrier Solutions, LLC</a> - All Rights Reserved.</p>
     </div>
         </footer>
         </div>
